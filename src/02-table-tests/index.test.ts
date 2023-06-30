ed5ce23ab99df8824dd4e9dd0656a5f90972db4d
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-30 11:38:00 +0300

    refactor: remove comments

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -12,9 +12,7 @@ const testCases = [
 ];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
   test.each(testCases)('Expect simpleCalculator({ $a, $b, action: "$action"}) to equal $expected', ({ a, b, action, expected }) => {
     expect(simpleCalculator({ a, b, action })).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
